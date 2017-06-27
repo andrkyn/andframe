274b580e5bfbe4cb051a3ec2dc3f6714e0145da3
andrkyn <andrhil14@gmail.com>
2017-06-27 11:10:51 +0300

    change small to big leater in class1

---
+++ src/Core/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Andframe\Framework\Core;
+
+
+ class Controller {
+
+     public function __construct()
+     {
+     echo 'Я нахожусь в папке andframe/core';
+     }
+
+     /*  public $model;
+       public $view;
+
+       function __construct()
+       {
+           $this->view = new View();
+       }
+
+       // действие (action), вызываемое по умолчанию
+       function action_index()
+       {
+           // todo
+       } */
+    }
+
+
+
+
